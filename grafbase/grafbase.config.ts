c07f455c747254049e981896ca3bd3778aeb66a2
Fuad <fuaddzakwan26@gmail.com>
2023-06-26 14:58:38 +0700

    mantap pertama

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,31 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+const User = g.model('User', {
+  name: g.string().length({
+    min: 2,
+    max: 20
+  }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  gitHubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(), // relasi kaya JOIN di SQL
+})
+
+const Project = g.model('Project', {
+  title: g.string().length({
+    min: 3
+  }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  gitHubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User)
+})
+
+export default config({
+  schema: g
+
+})
